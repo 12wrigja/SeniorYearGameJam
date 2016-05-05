427ed3c3c2acb6c186bb54c8ca828708053ab0de
Joseph Satterfield <jjs228@case.edu>
2016-05-04 23:12:06 -0400

    Standard particles and base weapons
    Signed-off-by: Joseph Satterfield <jjs228@case.edu>

---
+++ Assets/Scripts/ChargeWeapon.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class ChargeWeapon : Weapon {
+
+
+    public abstract void startCharge();
+    public abstract void endCharge();
+}
